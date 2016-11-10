23c01826f3cbefb8f3be41b04fbea651ca27b2ba
Nigel Robb <nigel.robb@ucd.ie>
2016-11-10 14:31:35 -0500

    Implement full POSM algorithm

--- posm/Posm.js
+++ posm/Posm.js
@@ -22,35 +22,63 @@ POSM.Posm.prototype.init = function(name, values) {
   return this[name].values[this.current];
 };
 
+/**
+ * Posm.prototype.update - finds the just right setting
+ *
+ * @param  {string} name        the name of the variable the settings are for
+ * @param  {string} observation an observation about the diffculty of the current
+ *                              setting
+ * @return {any}             the new setting
+ */
 POSM.Posm.prototype.update = function(name, observation) {
+  // find A (harder) and B (easier) for each setting
+  var harder = [];
+  var easier = [];
+  for (var setting in this[name].values) {
+    // get the total belief of all settings harder than the setting
+    var belief = 0;
+    for (var i = setting; i < this[name].values.length; i++) {
+      belief += this[name].belief[i];
+    }
+    harder[setting] = belief;
+    // get the total belief of all the settings easier than the setting
+    var belief = 0;
+    for (var j = setting; j >= 0; j--) {
+      belief += this[name].belief[j];
+    }
+    easier[setting] = belief;
+  }
+  // for each setting, which (easier or harder) has the LEAST total belief
+  var settings = [];
+  for (var belief in easier) {
+    if (easier[belief] <= harder[belief]) {
+      settings.push(easier[belief]);
+    } else {
+      settings.push(harder[belief]);
+    }
+  }
+  // now find which of these settings has the MOST belief
+  this.current = settings.indexOf(Math.max(...settings));
+  this.updateBelief(name, observation);
+  return this[name].values[this.current];
+}
+
+/**
+ * Posm.prototype.updateBelief - updates the belief of as many settings as possible
+ *
+ * @param  {string} name        the variable the setting are for
+ * @param  {string} observation an observation about the difficulty of the setting
+ */ 
+POSM.Posm.prototype.updateBelief = function(name, observation) {
   if (observation === POSM.TOO_HARD) {
     // update the belief of the current setting, and those more difficult
     for (var i = this.current; i < this[name].values.length; i++) {
       this[name].belief[i] *= POSM.LEARNING_RATE;
-    }
-    // get the next easiest setting with more belief
-    var currentBelief = this[name].belief[this.current];
-    for (var j = 0; j < this[name].values.length; j++) {
-      var belief = this[name].belief[j];
-      if (belief > currentBelief) {
-        this.current = j;
-      }
     }
   } else {
     // update the belief of the current setting, and those less difficult
     for (var i = this.current; i > -1; i--) {
       this[name].belief[i] *= POSM.LEARNING_RATE;
     }
-    // get the next more dificult setting with more belief
-    var currentBelief = this[name].belief[this.current];
-    for (var k = 0; k < this[name].values.length; k++) {
-      var belief = this[name].belief[k];
-      if (belief > currentBelief) {
-        this.current = k;
-        break;
-      }
-    }
   }
-  console.log(this.current);
-  return this[name].values[this.current];
-};
+}
