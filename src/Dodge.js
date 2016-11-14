1c0b11ef78806894e24b13dc6d71b5d4eed0e61f
Nigel Robb <nigel.robb@ucd.ie>
2016-11-14 11:59:58 -0500

    Add onHit and setVelocity functions

--- src/Dodge.js
+++ src/Dodge.js
@@ -7,8 +7,14 @@ DDATest.Dodge = function(game) {
 DDATest.Dodge.prototype.create = function() {
   // vars
   this.velocity = 1000;
+  this.fails = 0;
+  this.counter = 0;
   // physics
   this.physics.startSystem(Phaser.Physics.ARCADE);
+  // dda
+  // TODO
+  // juice
+  // TODO
   // wabbit
   this.player = this.add.sprite(this.world.centerX, 700, 'wabbit');
   this.player.anchor.setTo(0.5, 0.5);
@@ -17,8 +23,6 @@ DDATest.Dodge.prototype.create = function() {
   this.ball = this.add.sprite(this.rnd.integerInRange(0, this.world.width), 20, 'ball');
   this.ball.anchor.setTo(0.5, 0.5);
   this.physics.enable(this.ball, Phaser.Physics.ARCADE);
-  this.ball.body.onCollide = new Phaser.Signal();
-  this.ball.body.onCollide.add(this.resetBall, this);
   this.ball.checkWorldBounds = true;
   this.ball.events.onOutOfBounds.add(this.resetBall, this);
   this.resetBall();
@@ -26,13 +30,33 @@ DDATest.Dodge.prototype.create = function() {
 
 DDATest.Dodge.prototype.update = function() {
   this.player.x = this.input.x;
-  this.physics.arcade.overlap(this.player, this.ball);
+  this.physics.arcade.overlap(this.player, this.ball, this.onHit, null, this);
+};
+
+DDATest.Dodge.prototype.onHit = function() {
+  this.fails++;
+  //this.juicy.shake();
+  this.resetBall();
 };
 
 DDATest.Dodge.prototype.resetBall = function() {
-  var x = 0;
-  var y = 0;
-  // TODO make the ball start from any of N, S, E, W
+  this.setVelocity();
+  var x = this.rnd.integerInRange(-200, this.world.width + 200);
+  var y = this.rnd.integerInRange(-50, -10);
   this.ball.reset(x, y);
   this.physics.arcade.moveToXY(this.ball, this.player.body.x, this.player.body.y, this.velocity);
+};
+
+DDATest.Dodge.prototype.setVelocity = function() {
+  if (this.counter++ % 5 === 0) {
+    if (this.fails > 1) {
+      //TODO
+      //this.dda.update('velocity', POSM.TOO_HARD);
+    } else {
+      //TODO
+      //this.dda.update('velocity', POSM.TOO_EASY);
+    }
+    this.fails = 0;
+    this.counter = 0;
+  }
 };
